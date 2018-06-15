7bd77d2ef70d6370678c54421c21bdef1c69bd52
katsuya.ishiyama <katsuya.ishiyama@enish.com>
2018-06-15 19:20:57 +0900

    commit to share the code.

---
+++ similarity.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import json
+import nltk
+
+
+with open('test.json', 'r', encoding='utf-8') as f:
+	line = f.readline()
+	data = json.loads(line)
+
+description = data['description']
