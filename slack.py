a46350fc325153b7e7c185b8a066cacc7abc68ff
ENISH\katsuya.ishiyama <katsuya.ishiyama@enish.com>
2018-06-15 12:47:30 +0900

    add a script to post to slack

---
+++ slack.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import json
+import requests
+import yaml
+
+
+class Slack(object):
+    def __init__(self):
+        self._conf = self.load_conf()
+    
+    def load_conf(self) -> dict:
+        with open('.slack_conf.yaml', 'r') as f:
+            _conf = yaml.load(f)
+        return _conf
+    
+    def post_message(self, message: str) -> object:
+        url = self._conf['webhook']['url']
+        requests.post(url=url, data=json.dumps({'text': message}))
