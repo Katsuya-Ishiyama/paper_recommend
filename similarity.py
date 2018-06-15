42252004f0a31c9f6fbdb19a7aba4a248f8c793b
ENISH\katsuya.ishiyama <katsuya.ishiyama@enish.com>
2018-06-15 19:41:06 +0900

    replace tab to 4 spaces

--- similarity.py
+++ similarity.py
@@ -5,7 +5,7 @@ import nltk
 
 
 with open('test.json', 'r', encoding='utf-8') as f:
-	line = f.readline()
-	data = json.loads(line)
+    line = f.readline()
+    data = json.loads(line)
 
 description = data['description']
