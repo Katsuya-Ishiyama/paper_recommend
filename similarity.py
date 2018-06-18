7d51ba5d9a41da28366f3f8a6cd974a1c64f161d
ENISH\katsuya.ishiyama <katsuya.ishiyama@enish.com>
2018-06-18 13:13:23 +0900

    shareing the developing code.

--- similarity.py
+++ similarity.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import json
-import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 
-with open('test.json', 'r', encoding='utf-8') as f:
-    line = f.readline()
-    data = json.loads(line)
+def load_paper_summaries_from_json(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        paper_summaries = [json.loads(line) for line in f]
+    return paper_summaries
 
-description = data['description']
+
+test_query = 'I want to predict mascots\' popularity from its photo using machine learning methodologies.'
+
+descriptions = [test_query]
+paper_summaries = load_paper_summaries_from_json('.\\test\\test.json')
+for summary in paper_summaries:
+    descriptions.append(summary['description'])
