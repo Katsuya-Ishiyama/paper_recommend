19ea7ebfd4a814e2cf56ce2f9cea6bccfb746e23
ENISH\katsuya.ishiyama <katsuya.ishiyama@enish.com>
2018-06-19 20:08:51 +0900

    remove debug codes

--- similarity.py
+++ similarity.py
@@ -19,9 +19,6 @@ def calculate_similarity_of_interest(interest, descriptions):
     interest_tfidf_matrix = tfidf_matrix[0, :]
     descriptions_tfidf_matrix = tfidf_matrix[1:, :]
 
-#    from IPython import embed
-#    embed()
-
     similarity = cosine_similarity(X=interest_tfidf_matrix.reshape(1, -1),
                                    Y=descriptions_tfidf_matrix)
     return similarity[0].tolist()
