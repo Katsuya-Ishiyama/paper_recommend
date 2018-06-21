847842f0700d7abb893b7aca6afe0f3941a8127b
Katsuya Ishiyama <ishiyama.katsuya@gmail.com>
2018-06-22 01:11:10 +0900

    load_paper_summaries_from_json had been removed.

--- similarity.py
+++ similarity.py
@@ -5,12 +5,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-def load_paper_summaries_from_json(path: str):
-    with open(path, 'r', encoding='utf-8') as f:
-        paper_summaries = [json.loads(line) for line in f]
-    return paper_summaries
-
-
 def calculate_similarity_of_interest(interest, descriptions):
     corpus = interest + descriptions
     vectorizer = TfidfVectorizer(ngram_range=(1, 3),
